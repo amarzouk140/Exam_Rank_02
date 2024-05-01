9d7f5124d0e2af68b99b7b2e887bfdd26634631c
Ayman Marzouk <amarzouk@lab3r3s8.42abudhabi.ae>
2024-05-01 15:59:43 +0400

    fix split

--- level 4/ft_split/ft_split.c
+++ level 4/ft_split/ft_split.c
@@ -1,50 +1,76 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char *ft_strncpy(char *s1, char *s2, int n)
+char *ft_strncpy(char *s1, char *s2, int n) 
 {
-	int i = -1;
-
-	while (++i < n && s2[i])
-		s1[i] = s2[i];
-	s1[i] = '\0';
-	return (s1);
+    int i = 0;
+    while (i < n && s2[i]) {
+        s1[i] = s2[i];
+        i++;
+    }
+    while (i < n) {
+        s1[i++] = '\0';
+    }
+    return s1;
 }
 
-char	**ft_split(char *str)
+char **ft_split(char *str) 
 {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int wc = 0;
+    while (str[i]) 
+	{
+        while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+            i++;
+        if (str[i])
+            wc++;
+        while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+            i++;
+    }
+
+    char **out = (char **)malloc(sizeof(char *) * (wc + 1));
+    if (!out)
+        return NULL;
+
+    i = 0;
+    while (str[i]) {
+        while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+            i++;
+        j = i;
+        while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+            i++;
+        if (i > j) {
+            out[k] = (char *)malloc(sizeof(char) * (i - j + 1));
+            if (!out[k]) 
+			{
+                // Clean up previously allocated memory before returning NULL
+                while (k-- > 0) {
+                    free(out[k]);
+                }
+                free(out);
+                return NULL;
+            }
+            ft_strncpy(out[k], &str[j], i - j);
+            out[k][i - j] = '\0';  // Ensure null-termination
+            k++;
+        }
+    }
+    out[k] = NULL;
+    return out;
+}
+
+
+int main()
+{
+	char *str = "  Hello  World  this is ayman";
+	char **out = ft_split(str);
 	int i = 0;
-	int j = 0;
-	int k = 0;
-	int wc = 0;
-	while (str[i])
+	while (out[i])
 	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
-		if (str[i])
-			wc++;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
-			i++;
+		printf("%s\n", out[i]);
+		i++;
 	}
-	char **out = (char **)malloc(sizeof(char *) * (wc + 1));
-	if(!*out)
-		return NULL;
-	i = 0;
-	while (str[i])
-	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
-		j = i;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
-			i++;
-		if (i > j)
-		{
-			out[k] = (char *)malloc(sizeof(char) * ((i - j) + 1));
-			if(!out)
-				return NULL;
-			ft_strncpy(out[k++], &str[j], i - j);
-		}
-	}
-	out[k] = NULL;
-	return (out);
+	return 0;
 }
