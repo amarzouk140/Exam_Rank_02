b9e79088ffc5bca725e8f910401187e53dd01078
Ayman Marzouk <amarzouk@lab3r3s8.42abudhabi.ae>
2024-05-20 16:01:32 +0400

    fix rostring

--- level 4/rostring/rostring.c
+++ level 4/rostring/rostring.c
@@ -1,41 +1,40 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
-    int i;
-    int start;
-    int end;
-    int flag;
+void write_word(char *str, int start, int end) {
+    while (start < end)
+        write(1, &str[start++], 1);
+}
 
-    flag = 0;
-    if (argc > 1 && argv[1][0])
+int main(int argc, char **argv) {
+    int i = 0, start, end, flag = 0;
+
+    if (argc > 1 && argv[1][0]) 
     {
-        i = 0;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
+        while (argv[1][i] == ' ' || argv[1][i] == '\t') 
+        i++;  
         start = i;
-        while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
-            i++;
+        while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t') 
+        i++;
         end = i;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
-        while(argv[1][i]) 
+        while (argv[1][i] == ' ' || argv[1][i] == '\t') 
+        i++;
+
+        while (argv[1][i]) 
         {
-            while ((argv[1][i] == ' ' && argv[1][i + 1] == ' ') || (argv[1][i] == '\t' && argv[1][i + 1] == '\t'))
-                i++; 
-            if (argv[1][i] == ' ' || argv[1][i] == '\t')
-                flag = 1; 
-            write(1, &argv[1][i], 1);
-            i++;
+            if (argv[1][i] != ' ' && argv[1][i] != '\t') 
+            {
+                if (flag) 
+                write(1, " ", 1);
+                flag = 1;
+                while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+                    write(1, &argv[1][i++], 1);
+            } 
+            else
+                i++;
         }
-        if (flag)
-            write(1, " ", 1);
-        while (start < end)
-        {
-            write(1, &argv[1][start], 1);
-            start++;
-        }
+        if (flag) write(1, " ", 1);
+        write_word(argv[1], start, end);
     }
     write(1, "\n", 1);
-    return(0);
+    return 0;
 }
